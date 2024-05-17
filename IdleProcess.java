c399c2bf9ad7f44a387d2cb591335a0730af7c96
TristanPeloquin <tristanpeloquin@yahoo.com>
2024-05-17 18:01:04 -0400

    Removed duplicate files

--- IdleProcess.java
+++
@@ -1,9 +0,0 @@
-public class IdleProcess extends UserlandProcess{
-    void main() throws InterruptedException {
-        while(true){
-            OS.sleep(50);
-            cooperate();
-            Thread.sleep(50);
-        }
-    }
-}
--- Priority.java
+++
@@ -1,3 +0,0 @@
-public enum Priority {
-    REAL_TIME, INTERACTIVE, BACKGROUND
-}
